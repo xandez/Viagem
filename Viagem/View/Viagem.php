25f43afe458444fa52f8101fc0e66130f6a2b2fa
xandez <44793041+xandez@users.noreply.github.com>
2018-11-12 21:30:28 -0300

    .

--- Viagem/View/Viagem.php
+++ Viagem/View/Viagem.php
@@ -12,9 +12,12 @@
 	<link rel="stylesheet" type="text/css" href="../Css/Viagem.css">
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
 	<title>Viagens</title>
-	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script type="text/javascript" src="http://code.jquery.com/jquery-1.8.3.min.js"></script>
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" 
+	integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+		
 </head>
 <body>
 	<!-- header -->
@@ -22,6 +25,8 @@
 	  <span class="navbar-brand mb-0 h1">Viagens</span>
 	  <a class="btn btn-primary" data-toggle="modal" data-target="#exampleModalLong" href="#" role="button">Cadastrar Viagem</a>
 	</nav>
+	<span class="filtro">Filtrar Destino</span>
+	<input type="text" id="txtBusca" />
 	<!-- Tabela -->
 	<table class="table">
 	  <thead>
@@ -35,7 +40,7 @@
 	      <th scope="col">Status</th>
 	    </tr>
 	  </thead>
-	  <tbody>  	  
+	  <tbody id="filtro">  	  
 	<?php  
 	    $hora = date("d-m-Y");
 		$viagemcontroller = new ViagemController();
@@ -45,7 +50,7 @@
 			if ($lista != 0 ) {
 				foreach ($lista as $objeto) {
 					echo '<tr>
-					       <th scope="row">'.$objeto->id.'</th>
+					       <td>'.$objeto->id.'</td>
 					       <td>'.$objeto->local.'</td>
 					       <td>'.date("d-m-Y",strtotime($objeto->data)).'</td>
 					       <td>'.$objeto->viajante.'</td>
@@ -107,6 +112,28 @@
 	      </div>
 	    </div>
 	  </div>
-	</div>
+	</div>	
 </body>
 </html>
+	
+	<script type="text/javascript">/*
+			$(document).ready(function() {
+					alert("Você está executando a versão do jQuery: " + jQuery.fn.jquery);
+			});
+*/
+			$("#txtBusca").keyup(function(){
+				var texto = $(this).val();
+
+				//$("#filtro tr").css("display","block");
+				$("#filtro tr").each(function(){
+				//	alert($(this).text().indexOf(texto));
+					if($(this).text().indexOf(texto) < 0){
+						$(this).css("display","none");
+					}else{
+						$(this).css("display","");
+					}
+					
+				});
+			});
+			
+		</script>
